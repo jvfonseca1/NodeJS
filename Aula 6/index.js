321ef7bbd5f3d2363130f85e12866746f1756244
João Victor Fonseca <joao.fonseca.victor@gmail.com>
2022-05-08 22:04:19 -0300

    Handlebars

--- Aula 6/index.js
+++ Aula 6/index.js
@@ -1,6 +1,7 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const handlebars = require("express-handlebars");
+
 
 // Config
 // Template Engine
