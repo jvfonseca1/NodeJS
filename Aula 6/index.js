a86903efe8eb2207a9af15df0d70f9cfeba98896
João Victor Fonseca <joao.fonseca.victor@gmail.com>
2022-05-08 21:57:06 -0300

    Handlebars

--- Aula 6/index.js
+++ Aula 6/index.js
@@ -1,13 +1,12 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
-const handlebars = require ('express-handlebars');
+const handlebars = require("express-handlebars");
 
 // Config
-    // Template Engine
-        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-        app.set('view engine', 'handlebars')
+// Template Engine
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-
-app.listen(8081, function(){
-    console.log('Servidor rodando na url http://localhost:8081');
+app.listen(8081, function () {
+  console.log("Servidor rodando na url http://localhost:8081");
 });
